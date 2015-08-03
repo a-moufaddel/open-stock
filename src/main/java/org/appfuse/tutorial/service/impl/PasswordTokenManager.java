e39bb4fbf6a952e9736192e5f74aa262550c19d4
abdel7aq <abdelhaq.mouf@gmail.com>
2015-08-03 01:39:55 +0100

    Premier commit.

---
+++ src/main/java/org/appfuse/tutorial/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package org.appfuse.tutorial.service.impl;
+
+import org.appfuse.tutorial.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
